9d400d386eba98b6f4bbffca146888c5b4e5f6b7
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2026-05-02 01:37:44 +0600

    User register & Login works fine

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -35,6 +35,35 @@ async function userRegisterController(req, res) {
   });
 }
 
+async function userLoginController(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+  const isValidPass = await user.comparePassword(password);
+  if (!isValidPass) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+  res.cookie("token", token);
+  res.status(200).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+}
+
 module.exports = {
   userRegisterController,
+  userLoginController,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/register", authController.userRegisterController);
+router.post("/login", authController.userLoginController);
 
 module.exports = router;
